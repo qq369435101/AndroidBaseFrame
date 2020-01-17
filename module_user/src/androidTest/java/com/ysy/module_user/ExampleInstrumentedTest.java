24df69c338ce1d1026689df5cb3fde3c1f6280aa
yushengyang@pdmi.cn <YCAND1992>
2020-01-17 15:09:59 +0800

    code push

---
+++ module_user/src/androidTest/java/com/ysy/module_user/ExampleInstrumentedTest.java
@@ -0,0 +1,17 @@
+package com.ysy.module_user;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+//@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+//    @Test
+//    public void useAppContext() {
+//        // Context of the app under test.
+//        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+//
+//        assertEquals("com.ysy.module_user.test", appContext.getPackageName());
+//    }
+}
